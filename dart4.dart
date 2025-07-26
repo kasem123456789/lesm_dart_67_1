a6e678bb58ccc3420bd49d1df2beeb8319cab34b
kasem123456789 <6749010027@phuketvc.ac.th>
2025-07-26 15:08:06 +0700

    Step 4:

---
+++ dart4.dart
@@ -0,0 +1,12 @@
+// Dynamic Variable Type
+
+void main(){
+  dynamic dynamicVari =42;
+  print((dynamicVari));
+
+  dynamicVari="Hello,Dart!";
+  print(dynamicVari);
+
+  dynamicVari = [1,2,3];
+  print(dynamicVari);
+}
