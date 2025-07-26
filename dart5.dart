af12c7a3a2f93823c3eee38bf8696078687eb976
kasem123456789 <6749010027@phuketvc.ac.th>
2025-07-26 15:19:02 +0700

    Step 5:

---
+++ dart5.dart
@@ -0,0 +1,28 @@
+// Arithmetic operations in Dart
+
+void main(){
+  int a =5;
+  int b=10;
+
+  int difference = a-b;
+  print(difference);
+
+  int pro = a + b;
+  print(pro);
+
+  int quo = a / b;
+  print(quo);
+
+  int rem = a % b;
+
+
+
+  print("------------------");
+  int m =10;
+
+  print("**m = ${++m}");
+  print("m++ = ${m++}");
+  print("--m = ${m--}");
+  print("m-- =${--m}");
+}
+
