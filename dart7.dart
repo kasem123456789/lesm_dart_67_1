378bf4f79dbf6ac90000a90c1466ee2f599a51bd
kasem123456789 <6749010027@phuketvc.ac.th>
2025-07-26 15:43:35 +0700

    Step 7:

---
+++ dart7.dart
@@ -0,0 +1,18 @@
+// condition operators
+
+void main(){
+  var a = 10;
+  if (a>5)[
+    print("a is gre than 5");
+  ] else if (a == 0){
+    print("a is equal to 0");
+  } else {
+    print("a is not greater than 5");
+  }
+
+
+  // condition_expression
+  var age =20;
+  var Status =(age >=18) ?"Adult" : "young";
+  print("Status: $Status");
+}
