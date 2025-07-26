be52f600b26310ffcfbfdcd8c9512252f0c607eb
kasem123456789 <6749010027@phuketvc.ac.th>
2025-07-26 15:01:51 +0700

    Step 3:

---
+++ dart3.dart
@@ -0,0 +1,27 @@
+// Static Variadle Type
+void main(){
+  //Sensitive Case
+  Stream name =  "Alice";
+  Stream _name = "BOb";
+  Stream nAme =  "Charli";
+
+  print(name);
+  print(nAme);
+  print(_name);
+
+  print("-------");
+
+  name = "david";
+  int age =20;
+  double score = 95.5;
+  num heighi 147, weight =70;
+  bool isStudent = true;
+
+  print(name);
+    print(age);
+      print(score);
+        print(heighi);
+          print(weight);
+          print(isStudent);
+
+}
