1e8c69e059c6a5198eef8c730c3ec8b036f25f81
kasem123456789 <6749010027@phuketvc.ac.th>
2025-08-02 14:58:38 +0700

    dart15

---
+++ dart15.dart
@@ -0,0 +1,19 @@
+// Switch Case
+
+void main(){
+  int number = 2;
+
+  switch (number){
+    case 1:
+    print("Number is one");
+    break;
+    case 2:
+    print("Number is two");
+    break;
+    case 3:
+    print("Number is three");
+    break;
+    default:
+    print("Number is one, two ,or three");
+  }
+}
